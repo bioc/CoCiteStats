c717d0901dde8602e26964add7a5aa94c13090b3
Robert Gentleman <rgentlem@fhcrc.org>
2007-06-07 16:48:37 +0000

    more fixing and updating of man pages and code
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CoCiteStats@25252 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/twTStats.R
+++ R/twTStats.R
@@ -1,15 +1,24 @@
 twTStats <- function (twT) 
 {
+    if(length(twT) != 4 )
+       stop("only 2 by 2 tables can be handled")
+    #if it is a matrix then it is in the wrong order
+    #and we rearrange it
+    if( is.matrix(twT) )
+      twT = c(twT[1,], twT[2,])
+
     twT = as.double(twT)
     m1 = twT[1] + twT[2]
     m2 = twT[1] + twT[3]
     M = sum(twT)
     ##check to see if either citation count is zero
     if( (m1 == 0) || (m2 == 0 ) )
-      return(c(Concordance = 0, Jaccard  = 0, Hubert = 0)) 
+      return(c(OddsRatio = 1, Concordance = 0, Jaccard  = 0, Hubert = 0)) 
     else
-     return(c(Concordance = twT[1], Jaccard = twT[1]/(sum(twT[1:3])), 
-        Hubert = (M * twT[1] - m1 * m2)/sqrt(m1 * m2 * (M - m1) * 
-            (M - m2))))
+     return(c(OddsRatio = (twT[1]*twT[4])/(twT[2]*twT[3]), 
+              Concordance = twT[1], 
+              Jaccard = twT[1]/(sum(twT[1:3])), 
+              Hubert = (M * twT[1] - m1 * m2)/sqrt(m1 * m2 * (M - m1) * 
+                           (M - m2))))
 }
 
