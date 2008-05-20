94bbea04756115c50ecc9a749063b74e64825a5a
Robert Gentleman <rgentlem@fhcrc.org>
2008-05-20 21:59:27 +0000

    tried to rework some of this to make better use of the AnnotationDbi interface
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CoCiteStats@31823 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/paperLen.R
+++ R/paperLen.R
@@ -1,16 +1,12 @@
 paperLen <- function (x) 
 {
-    if(missing(x)) 
-       x = Lkeys(org.Hs.egPMID) 
-
-    papersByLL = mget(x, org.Hs.egPMID, ifnotfound = NA)
-    papers = unique(unlist(papersByLL))
-    inap = is.na(papers) | papers == "NA"
-    if (any(inap)) 
-        papers = papers[!inap]
-    if (length(papers) == 0) 
+    tT = toTable(org.Hs.egPMID)
+    papersByLL = split(tT[,2], tT[,1])
+    if( !missing(x) ) papersByLL = papersByLL[x]
+    if (length(papersByLL) == 0) 
         return(list(counts = numeric(0), papers = numeric(0)))
-    paperForLL = mget(papers, revmap(org.Hs.egPMID))
-    paperCts = sapply(paperForLL, length)
+    paperCts = nhit(revmap(org.Hs.egPMID))
+    if( !missing(x) )
+	paperCts = paperCts[unique(unlist(papersByLL, use.names=FALSE))]
     return(list(counts = paperCts, papers = papersByLL))
 }
